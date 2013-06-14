525ec3111528364aa6b4eaed9b7fa6c77a142b42
weyewe <w.yunnal@gmail.com>
2013-06-14 09:05:05 +0700

    added pagination, only next

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,6 @@ class PostsController < ApplicationController
     
     
     
-    @posts = Post.joins(:pics, :tag).page(params[:page]).per( 10 ).order("id DESC")
+    @posts = Post.joins(:pics, :tag).page(params[:page]).per( 20 ).order("id DESC")
   end
 end
